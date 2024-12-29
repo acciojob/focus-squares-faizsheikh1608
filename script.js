53cf55d5daf92ed4a6abb0aa2999db94bba4633b
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-29 12:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((sqr) => {
+	sqr.addEventListener('mouseover',function(e){
+		squares.forEach((sqr) => {
+			if(sqr !== e.target){
+				sqr.style.backgroundColor = '#6F4E37';
+			}
+		})
+	})
+
+	sqr.addEventListener('mouseout',function(e) {
+	squares.forEach((sqr) =>{
+		sqr.style.backgroundColor = '#E6E6FA';
+	})
+	})
+})
